02022d659a1d2e0765897948ef45986e610f90ba
dehanalkautsar <dehanalkautsar3@gmail.com>
2021-07-26 21:46:06 +0700

    add pick color tool

---
+++ color.js
@@ -0,0 +1,11 @@
+// NIM  : 13519200
+// Nama : Muhammad Dehan Al Kautsar
+// Tugas Ca-Gaib 2021
+
+class Color {
+     constructor(red, green, blue) {
+          this.red = red;
+          this.green = green;
+          this.blue = blue;
+     }
+}
